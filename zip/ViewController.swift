2af94e6dc952e38f5273b1d2c6572760c152987f
Martin Skogum <skogum1@outlook.com>
2016-10-09 22:54:16 +0200

    My first commit

--- zip/ViewController.swift
+++ zip/ViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var TheLabel: UILabel!
 
+    var tapCount = 0
+    
+    @IBAction func buttontapped(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        
+        if tapCount >= 10 {
+            TheLabel.text = "Nice 10 Times!"
+         }
+      }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
     override func didReceiveMemoryWarning() {
